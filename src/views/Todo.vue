3a75fbfef3323128b60d61e205dab2113e6dffbf
saeedmaldosary <saeedmaldosary@gmail.com>
2021-06-13 12:26:36 +0300

    Adding remaining and completed counter

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,54 +1,91 @@
 <template>
-    <v-container>
-  <v-row align="center"
-      justify="center"
-      class="mt-1">
-      <v-col cols="12"
-        md="6"
-        lg="8" style="background-color:#f9f9f9;">
-  <div class="home justify-center" >
-    
-    <v-text-field
-      outlined
-      v-model="newTaskTitle"
-      class="pa-3"
-      label="Add task"
-      hide-details
-      @click:append="addTask"
-      @keyup.enter="addTask"
-      append-icon="mdi-plus-circle-outline"
-    />
-    <v-list subheader two-line flat class="pt-0">
-      <div v-for="task in filteredTasks">
-        <v-list-item
-          @click="doneTask(task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
+  <v-container>
+    <v-row align="center" justify="center" class="mt-1">
+      <v-col cols="12" md="6" lg="8" style="background-color:#f9f9f9;">
+        <div class="home justify-center">
 
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.title }}</v-list-item-title
+
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      
+       
+
+
+    <v-row
+      class="my-1"
+      align="center"
+    >
+      <strong class="mx-6 info--text text--darken-2 font-weight-light">
+        Remaining: {{ remainingTasks }}
+      </strong>
+
+      <v-divider vertical></v-divider>
+
+      <strong class="mx-4 success--text text--darken-2 font-weight-light">
+        Completed: {{ completedTasks }}
+      </strong>
+
+      <v-spacer></v-spacer>
+      </v-row>
+
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+      <!--  -->
+          <v-text-field
+            outlined
+            v-model="newTaskTitle"
+            class="pa-3"
+            label="Add task"
+            hide-details
+            @click:append="addTask"
+            @keyup.enter="addTask"
+            append-icon="mdi-plus-circle-outline"
+          />
+          <v-list subheader two-line flat class="pt-0">
+            <div v-for="task in filteredTasks">
+              <v-list-item
+                @click="doneTask(task.id)"
+                :class="{ 'blue lighten-5': task.done }"
               >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn icon @click.stop="deleteTask(task.id)">
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
-  </div>
-  </v-col>
-  </v-row>
-</v-container>
+                <template>
+                  <v-list-item-action>
+                    <v-checkbox
+                      :input-value="task.done"
+                      color="primary"
+                    ></v-checkbox>
+                  </v-list-item-action>
+
+                  <v-list-item-content>
+                    <v-list-item-title
+                      :class="{ 'text-decoration-line-through': task.done }"
+                      >{{ task.title }}</v-list-item-title
+                    >
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-btn icon @click.stop="deleteTask(task.id)">
+                      <v-icon color="primary lighten-1">mdi-delete</v-icon>
+                    </v-btn>
+                  </v-list-item-action>
+                </template>
+              </v-list-item>
+              <v-divider></v-divider>
+            </div>
+          </v-list>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -87,7 +124,15 @@ export default {
       return this.tasks.filter((task) => {
         return task.title.match(this.search);
       });
+      
     },
+       completedTasks () {
+        return this.tasks.filter(task => task.done).length
+      },
+     
+      remainingTasks () {
+        return this.tasks.length - this.completedTasks
+      },
   },
   methods: {
     doneTask(id) {
@@ -108,6 +153,8 @@ export default {
         this.newTaskTitle = "";
       }
     },
+
+
   },
 };
 </script>
